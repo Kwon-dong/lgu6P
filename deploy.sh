5ff8bbae4f11a4a099888f006348118c9e889a6d
Kwon-dong <glue68@naver.com>
2025-04-11 10:06:27 +0900

    updated | 2025-04-11 01:06:27 (KST)

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,15 @@
-now=$(date '+%Y-%m-%d %H:%M:%S')
+#!/bin/bash
+
+# 사용자로부터 커밋 메시지 입력받기
+read -p "커밋 메시지를 입력하세요: " user_message
+
+# 현재 KST 시간 구하기 (UTC+9)
+current_time=$(TZ=Asia/Seoul date '+%Y-%m-%d %H:%M:%S (KST)')
+
+# 메시지 조합
+full_message="$user_message | $current_time"
+
+# Git 커밋 및 푸시
 git add .
-git commit -m "updated: $now"
+git commit -m "$full_message"
 git push
