805b0d7e516e66cebcf50b65ba1206a0895af09c
Kwon-dong <glue68@naver.com>
2025-04-21 10:28:44 +0900

    testing | 2025-04-21 01:28:44 (KST)

---
+++ docker_250414.py
@@ -0,0 +1,12 @@
+# Docker Version 확인
+
+'''
+$ sudo docker version
+'''
+
+# 이번주 수업 내용
+- 도커 기본 명령어
+- 다양한 컨테이너 실습 : Python, django, Nginx, etc...
+- Docker 네트워크, Volume, etc..
+- Dockerfile 만드는 것이 핵심
+- Docker Compose까지 수업 진행
